00e3eec50aab468576756ff13e20a25f88386c62
Daniel Dolejška <dolejskad@gmail.com>
2019-12-16 11:53:43 +0100

    Added app executable building script

---
+++ build.ps1
@@ -0,0 +1,1 @@
+pipenv run pyinstaller --name "Camp_Room_CSP" --add-data "ui;ui" --noconsole run.py
--- room_csp/ui/__init__.py
+++ room_csp/ui/__init__.py
@@ -2,4 +2,7 @@ from .setup import setup_and_run_ui
 
 __all__ = [
     "setup_and_run_ui",
+    "models",
+    "views",
+    "windows",
 ]
